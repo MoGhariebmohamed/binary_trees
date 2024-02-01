8b4f5c53197bcee3106a5fc84dfdca6cc91a51b1
mohamed gharieb <m.gharieb89@gmail.com>
2024-02-01 15:42:21 -0800

    t max v99

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,14 +7,16 @@
  * binary_tree_depth - Measures the depth of a node in a binary tree.
  * @tree: A pointer to the node for which to measure the depth.
  *
- * Return: The depth of the node, 0 if tree is NULL or if the node has no parent.
+ * Return: depth of the node, 0 if tree is NULL or if the node has no parent
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    if (tree == NULL || tree->parent == NULL)
-        return (0);
-    else
-    {
-        return (1 + binary_tree_depth(tree->parent));
-    }
+	if (tree == NULL || tree->parent == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+	return (1 + binary_tree_depth(tree->parent));
+	}
 }
--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -4,20 +4,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - Counts the nodes with at least one child in a binary tree.
- * @tree: A pointer to the root node of the tree.
+ *binary_tree_nodes - Counts the nodes with at least one child in a binary tree
+ *@tree: A pointer to the root node of the tree.
  *
- * Return: The number of nodes with at least one child, 0 if tree is NULL.
+ *Return: The number of nodes with at least one child, 0 if tree is NULL.
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    else
-    {
-        if (tree->left == NULL && tree->right == NULL)
-            return (0);
-        else
-            return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree->left == NULL && tree->right == NULL)
+			return (0);
+		return (1 + binary_tree_nodes(tree->left) +
+				binary_tree_nodes(tree->right));
+	}
 }
